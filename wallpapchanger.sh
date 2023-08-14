20494d750ce1e02c2d38415f7020ffe905d5393a
nuklearwarlust <141370416+nuklearwarlust@users.noreply.github.com>
2023-08-14 14:53:04 -0500

    Update wallpapchanger.sh
    added notes

--- wallpapchanger.sh
+++ wallpapchanger.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
+# defines username and image file
 export USER
 export IMAGE
 echo "wallpaper changer"
 sleep 1
+# reads username, goes to pictures directory and then list files
 echo "enter username: "
 read USER
 cd /home/$USER/Pictures/ && ls
+# reads image name and sets image as background
 echo "enter file name: "
 read IMAGE && gsettings set org.gnome.desktop.background picture-uri-dark file:///home/$USER/Pictures/$IMAGE
 echo "image set!"
